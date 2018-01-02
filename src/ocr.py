1b9474a0368c50584353f4bb41fd41e45a7aed25
David Valpey <dvalpey@jibes.nl>
2018-01-01 19:27:41 -0800

    Add method to load images

--- src/ocr.py
+++ src/ocr.py
@@ -1,11 +1,21 @@
 import pytesseract
+from pathlib import Path
 from PIL import Image
 
 
-def run_ocr(files):
+def run_ocr(imgs: list):
     text_pages = []
-    for f in files:
-        with Image.open(f) as img:
-            text_pages.append(pytesseract.image_to_string(img))
+    for img in imgs:
+        text_pages.append(pytesseract.image_to_string(img))
 
     return '\n'.join(text_pages)
+
+
+def open_images(filename: str):
+    image_dir = Path('../data/images')
+    filenames = sorted(image_dir.glob(filename))
+    imgs = []
+    for f in filenames:
+        imgs.append(Image.open(f))
+
+    return imgs
