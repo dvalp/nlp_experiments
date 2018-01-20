027008a023448b98be5e9af8e94e5746b2ac415a
David Valpey <dvalpey@jibes.nl>
2018-01-20 10:30:03 +0100

    Add dummy variables and spacy analysis, change dtaframe variable to df

--- src/sentence_classification.py
+++ src/sentence_classification.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import spacy
 
 def build_nlp_dataframe():
-    df_legal_system = pd.read_csv("./data/sentences_legal_system_type.csv", index_col=False)
-    df_legal_system = df_legal_system.append(
-        pd.read_csv("./data/sentences_legal_system_type_with_negative_samples.csv", index_col=False))
+    df = pd.read_csv("../data/sentences_legal_system_type.csv", index_col=False)
+    df = df.append(
+        pd.read_csv("../data/sentences_legal_system_type_with_negative_samples.csv", index_col=False))
+    df = pd.concat([df, pd.get_dummies(df[['Taal', 'Type']])], axis=1)
 
-    return df_legal_system
+    for lang in df['Taal'].unique():
+        nlp = spacy.load(lang)
+        df.loc[df['Taal'] == lang, 'Spacy_Docs'] = df.loc[df['Taal'] == lang, 'Sentence'].apply(nlp)
+    return df
