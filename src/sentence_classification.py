eda7067a0053e8f2be4834fa7f6fc09033e286d3
David Valpey <dvalpey@jibes.nl>
2018-01-20 23:17:30 +0100

    Create columns for stop words and text w/o stop words

--- src/sentence_classification.py
+++ src/sentence_classification.py
@@ -1,3 +1,4 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import spacy
 
@@ -11,4 +12,7 @@ def build_nlp_dataframe():
     for lang in df['Taal'].unique():
         nlp = spacy.load(lang)
         df.loc[df['Taal'] == lang, 'Spacy_Docs'] = df.loc[df['Taal'] == lang, 'Sentence'].str.strip().apply(nlp)
+
+    df['No_Stops'] = df['Spacy_Docs'].apply(lambda doc: [w for w in doc if not w.is_stop and not w.is_punct])
+    df['Stops'] = df['Spacy_Docs'].apply(lambda doc: [w for w in doc if w.is_stop])
     return df
