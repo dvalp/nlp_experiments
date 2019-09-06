d840a7d777e8017a037d6e46d73d54f6c646e66a
David Valpey <dvalpey@jibes.nl>
2019-09-06 20:52:24 +0200

    Add function to create JSON sample files, add comments for dataframe code

--- src/russian_tweets.py
+++ src/russian_tweets.py
@@ -1,10 +1,31 @@
+import json
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 
 
-def build_tweet_df() -> pd.DataFrame:
+def build_tweet_df(filter_language: int = 'English', convert_dates=True) -> pd.DataFrame:
+    """
+    Create a DataFrame from directory of Russian troll tweets.
+
+    Defaults to filtering only for documents labeled as English language, but
+    many other languages are present in the original data. Not all language
+    fields are completely correct - I've noticed some that appear to be English
+    but are marked as unknown languages.
+
+    Setting column types stops Pandas from guessing; guessing causes problems
+    when combining some of the data sets. Converting to categoricals and dates
+    makes the data more useful in some contexts.
+
+    Datetime fields are not storable in JSON, set convert_dates to False if
+    you intend to make JSON files from this dataset using the function
+    make_json_samples() below.
+
+    :param filter_language: Language to filter on
+    :param convert_dates: Convert dates to datetime
+    :return: DataFrame of twitter posts with metadata
+    """
     col_types = {'external_author_id': str,
                  'author':             str,
                  'content':            str,
@@ -31,13 +52,37 @@ def build_tweet_df() -> pd.DataFrame:
         (pd.read_csv(csv, header=0, dtype=col_types) for csv in Path('../data/russian-troll-tweets').rglob('*.csv')),
         ignore_index=True)
 
-    df_rus = df_rus[df_rus['language'] == 'English'].dropna(subset=['content'])
+    df_rus = df_rus[df_rus['language'] == filter_language].dropna(subset=['content'])
 
-    df_rus['publish_date'] = pd.to_datetime(df_rus['publish_date'], infer_datetime_format=True)
-    df_rus['harvested_date'] = pd.to_datetime(df_rus['harvested_date'], infer_datetime_format=True)
+    if convert_dates:
+        df_rus['publish_date'] = pd.to_datetime(df_rus['publish_date'], infer_datetime_format=True)
+        df_rus['harvested_date'] = pd.to_datetime(df_rus['harvested_date'], infer_datetime_format=True)
+
     categorical_fields = ['region', 'language', 'post_type', 'account_type', 'account_category']
     df_rus[categorical_fields] = df_rus[categorical_fields].astype('category')
 
     return df_rus
 
 
+def make_json_samples(df: pd.DataFrame, sample_size=20) -> None:
+    """
+    Make a small set of JSON files from the tweet collection. This is only
+    necessary for creating some sample documents for testing some code to
+    read json files into a database.
+
+    Note that the URLs are artificially moved into a sub group for testing
+    the code with nested JSON.
+
+    :param df: Tweet DataFrame created by build_tweet_df() (above)
+    :param sample_size: Number of items to return
+    """
+    docs = df.sample(n=sample_size).to_dict(orient='records')
+
+    for doc in docs:
+        doc['urls'] = {}
+        for field in ['article_url', 'tco1_step1', 'tco2_step1', 'tco3_step1']:
+            doc['urls'][field] = doc[field]
+            del doc[field]
+
+        with open(Path('../data/json_tweets/', doc['tweet_id']), 'w') as fp:
+            json.dump(doc, fp)
