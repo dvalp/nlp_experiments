e6cd63da0c6bba6837841a8378a4b9a9f8d134e2
David Valpey <david.valpey@viqtordavis.com>
2020-01-15 14:55:22 +0100

    Add code to process rechtspraak document xml

--- src/xml_processing.py
+++ src/xml_processing.py
@@ -1,5 +1,55 @@
+from pathlib import Path
+
 from lxml import etree
 
 
-def read_xml():
-    etree
+def extract_rechtspraak_xml(filename: str):
+    root = etree.parse(filename).getroot()
+    descriptions = root.find('.//{*}RDF').getchildren()
+
+    document_info = elem2dict(descriptions.pop(0))
+    for desc in descriptions:
+        document_info.update({key: value for key, value in elem2dict(desc).items() if key not in document_info})
+
+    document_info['abstract'] = [text.strip() for text in root.find('.//{*}inhoudsindicatie').itertext()
+                                 if text.strip()]
+    document_info['hasVersion'] = [text.strip() for text in root.find('.//{*}hasVersion').itertext() if text.strip()]
+
+    if root.find('.//{*}uitspraak') is not None:
+        document_info['text'] = [text.strip() for text in root.find('.//{*}uitspraak').itertext() if text.strip()]
+
+    elif root.find('.//{*}conclusie') is not None:
+        document_info['text'] = [text.strip() for text in root.find('.//{*}conclusie').itertext() if text.strip()]
+    else:
+        print(filename)
+
+    return document_info
+
+
+def read_xmls():
+    xml_paths = Path("data/sample_dataset/xmls/").rglob("*.xml")
+    return [extract_rechtspraak_xml(str(document)) for document in xml_paths]
+
+
+def elem2dict(node: etree) -> dict:
+    """
+    Convert an lxml.etree node tree into a dict.
+
+    Does not work well on some types, ie. lists, references to other parts of
+    the documents or duplicate tags, so extra steps are still needed.
+
+    Based on https://gist.github.com/jacobian/795571
+
+    :param node: lxml.etree Element
+    :return: Dictionary containing the children of the original node.
+    """
+    d = {}
+    for element in node.iterchildren():
+        key = etree.QName(element).localname
+        value = element.text if element.text else elem2dict(element)
+        d[key] = value
+    return d
+
+
+if __name__ == '__main__':
+    read_xmls()
