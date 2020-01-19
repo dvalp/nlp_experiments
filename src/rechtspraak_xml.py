f6db7193ba997a57c8c6fe8cdd28e202cc17e0b7
David Valpey <david.valpey@viqtordavis.com>
2020-01-19 01:58:40 +0100

    Add docstrings and improve variable name

--- src/rechtspraak_xml.py
+++ src/rechtspraak_xml.py
@@ -1,3 +1,13 @@
+"""
+Extract text and metadata from xml downloaded from rechspraak.nl
+
+Elements are extracted and added to the document metadata. This can also be
+run as a batch, in which case it returns a generator for iterating through
+results.
+
+TODO: The recursive function that makes a dictionary does not handle duplicate
+    fields well.
+"""
 import pickle
 from collections import defaultdict
 from pathlib import Path
@@ -5,7 +15,14 @@ from pathlib import Path
 from lxml import etree
 
 
-def extract_rechtspraak_xml(filename: str):
+def extract_rechtspraak_xml(filename: str) -> dict:
+    """
+    The main work of extracting information from an xml is done here. Improving
+    access to the metadata by fields would help a lot.
+
+    :param filename: Location to the instance of the xml for extraction
+    :return: Dictionary of metadata extracted from the xml file
+    """
     root = etree.parse(filename).getroot()
     descriptions = root.find(".//{*}RDF").getchildren()
 
@@ -58,6 +75,12 @@ def extract_rechtspraak_xml(filename: str):
 
 
 def read_xmls(xml_dir="data/sample_dataset/xmls/"):
+    """
+    Simple generator to handle bulk processing of the files.
+
+    :param xml_dir: Name of the directory to process.
+    :return: Dictionary of results from files found in a directory.
+    """
     xml_paths = Path(xml_dir).rglob("*.xml")
     for document in xml_paths:
         yield extract_rechtspraak_xml(str(document))
@@ -65,7 +88,7 @@ def read_xmls(xml_dir="data/sample_dataset/xmls/"):
 
 def elem2dict(node: etree) -> dict:
     """
-    Convert an lxml.etree node tree into a dict.
+    Convert an lxml.etree node tree into a dict using recursion.
 
     Does not work well on some types, ie. lists, references to other parts of
     the documents or duplicate tags, so extra steps are still needed.
@@ -75,12 +98,12 @@ def elem2dict(node: etree) -> dict:
     :param node: lxml.etree Element
     :return: Dictionary containing the children of the original node.
     """
-    d = {}
+    doc_imetadata = {}
     for element in node.iterchildren():
         key = etree.QName(element).localname
         value = element.text.strip() if element.text else elem2dict(element)
-        d[key] = value
-    return d
+        doc_imetadata[key] = value
+    return doc_imetadata
 
 
 if __name__ == "__main__":
