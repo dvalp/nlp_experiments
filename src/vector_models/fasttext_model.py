8423d45cf5a373598f956d0430d065af97116970
David Valpey <david.valpey@viqtordavis.com>
2020-04-07 00:28:45 +0200

    Add streaming bulk for ingest of documents, update requirements.txt, update index schema

--- src/vector_models/fasttext_model.py
+++ src/vector_models/fasttext_model.py
@@ -2,9 +2,13 @@ from typing import Collection
 
 import numpy as np
 from bs4 import BeautifulSoup
+from elasticsearch.helpers import streaming_bulk
 from gensim.models import FastText
 from gensim.summarization.textcleaner import get_sentences
 from gensim.utils import simple_preprocess
+from tqdm import tqdm
+
+from elastic.elasticsearch_connection import INDEX_NAME, ES_CONTEXT
 
 
 def train_model(sentences: Collection[str]):
@@ -25,14 +29,35 @@ def create_training_sentences(html_pages: Collection[str]):
 
 
 def vectorize_text(page: str, ft_model: FastText):
-
     tokens = simple_preprocess(page)
     vector = np.zeros(100)
-    for token in tokens:
-        vector += ft_model.wv[token]
-    return vector / len(tokens)
+
+    if tokens:
+        for token in tokens:
+            vector += ft_model.wv[token]
+        return vector / len(tokens)
+
+    return vector
+
+
+def generate_data(pages, ft_model):
+    for page in pages:
+        yield {
+            "_index": INDEX_NAME,
+            "_op_type": "index",
+            "page_text": page,
+            "page_vector": list(vectorize_text(page, ft_model))}
 
 
 def strip_tags(html_pages: Collection[str]):
     for page in html_pages:
         yield BeautifulSoup(page, "lxml").text
+
+
+def bulk_index(document_set, ft_model):
+    progress = tqdm(unit="pages", total=len(document_set))
+    successes = 0
+    for ok, action in streaming_bulk(client=ES_CONTEXT, actions=generate_data(document_set, ft_model)):
+        progress.update(1)
+        successes += ok
+    print(f"Indexed {successes}/{len(document_set)} documents")
