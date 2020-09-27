eed6f80260b1a116426aedaeda4812a7b7ef2261
David Valpey <david.valpey@viqtordavis.com>
2020-09-28 00:15:38 +0200

    Read CSV for Russian Tweets

--- src/dataset_readers/russian_tweets_reader.py
+++ src/dataset_readers/russian_tweets_reader.py
@@ -1,22 +1,33 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
 from data_structures.russian_tweet_data import RussianTweetData
 
 TWEET_DIR = "../data/russian-troll-tweets"
+TWEET_FILE_EXTENSION = "csv"
 
 
-class DatasetReader:
-    def __init__(self, document_location):
+class DatasetReader(ABC):
+    def __init__(self, document_location: str, document_extension: str):
         self.document_location: str = document_location
+        self.document_extension: str = document_extension
 
     def load_documents(self):
-        pass
+        for fp in Path(self.document_location).rglob(f"*.{TWEET_FILE_EXTENSION}"):
+            self.convert_document(fp)
 
-    def convert_document(self):
+    @abstractmethod
+    def convert_document(self, fp: Path):
         raise NotImplementedError
 
 
 class RussianTweetReader(DatasetReader):
-    def __init__(self, document_location: str = TWEET_DIR):
-        super().__init__(document_location)
+    def __init__(
+            self,
+            document_location: str = TWEET_DIR,
+            document_extension: str = TWEET_FILE_EXTENSION
+    ):
+        super().__init__(document_location, document_extension)
 
-    def convert_document(self):
+    def convert_document(self, fp: Path):
         pass
