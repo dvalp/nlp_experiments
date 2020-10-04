0e6d98c954217de378ea4db678c5f4779615acb1
David Valpey <david.valpey@viqtordavis.com>
2020-10-04 23:58:56 +0200

    Try to make load_documents() return individual records using generator

--- src/dataset_readers/dataset_reader.py
+++ src/dataset_readers/dataset_reader.py
@@ -1,3 +1,4 @@
+import itertools
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import NamedTuple
@@ -9,8 +10,10 @@ class DatasetReader(ABC):
         self.document_extension: str = document_extension
 
     def load_documents(self, file_extension: str) -> NamedTuple:
-        for fp in Path(self.document_location).rglob(f"*.{file_extension}"):
-            yield self.convert_document(fp)
+        data_files = Path(self.document_location).rglob(f"*.{file_extension}")
+        entries = itertools.chain(self.convert_document(fp) for fp in data_files)
+        for entry in entries:
+            yield entry
 
     @abstractmethod
     def convert_document(self, fp: Path):
