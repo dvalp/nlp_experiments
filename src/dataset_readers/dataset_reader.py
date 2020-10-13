19732d5c9fe3db0754bf7fbb11e624f73087e080
David Valpey <david.valpey@viqtordavis.com>
2020-10-14 01:30:25 +0200

    Remove convert to list

--- src/dataset_readers/dataset_reader.py
+++ src/dataset_readers/dataset_reader.py
@@ -9,7 +9,7 @@ class DatasetReader(ABC):
     def __init__(self, document_location: str, document_extension: str):
         self.document_location: str = document_location
         self.document_extension: str = document_extension
-        self.data_files = list(Path(self.document_location).rglob(f"*.{self.document_extension}"))
+        self.data_files = Path(self.document_location).rglob(f"*.{self.document_extension}")
         self.data_points = itertools.chain(*[self.convert_document(fpath) for fpath in self.data_files])
 
     @abstractmethod
