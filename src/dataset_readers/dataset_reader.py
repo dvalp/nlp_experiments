649dd9c011fd3f51cc3938cc8b9e338ca629fa41
David Valpey <david.valpey@viqtordavis.com>
2020-10-14 02:27:28 +0200

    Remove tweet dependency, change iterator to iterable

--- src/dataset_readers/dataset_reader.py
+++ src/dataset_readers/dataset_reader.py
@@ -2,8 +2,6 @@ import itertools
 from abc import ABC, abstractmethod
 from pathlib import Path
 
-from data_structures.russian_tweet_data import RussianTweetData
-
 
 class DatasetReader(ABC):
     def __init__(self, document_location: str, document_extension: str):
@@ -17,7 +15,5 @@ class DatasetReader(ABC):
         pass
 
     def __iter__(self):
-        return self
-
-    def __next__(self) -> RussianTweetData:
-        return next(self.data_points)
+        for record in self.data_points:
+            yield record
