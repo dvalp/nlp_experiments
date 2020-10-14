c81ea3256452d6bb4f58f53eca10bb6c3d62b7bb
David Valpey <david.valpey@viqtordavis.com>
2020-10-15 00:49:42 +0200

    Turn class into context manager

--- src/dataset_readers/dataset_reader.py
+++ src/dataset_readers/dataset_reader.py
@@ -1,5 +1,14 @@
-import itertools
+"""
+Abstract Base Class for reading document sets into an internal data model.
+
+Designed to be used as an iterator so that individual records can be collected
+and potentially batched together for processing.
+
+Optionally to be used as a context manager in cases where setup/teardown are
+required (if reading from a stream for example.
+"""
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 from pathlib import Path
 
 
@@ -7,19 +16,17 @@ class DatasetReader(ABC):
     def __init__(self, document_location: str, document_extension: str):
         self.document_location: str = document_location
         self.document_extension: str = document_extension
-        self.data_files = Path(self.document_location).rglob(f"*.{self.document_extension}")
-        self.data_points = itertools.chain(*[self.convert_document(fpath) for fpath in self.data_files])
 
     @abstractmethod
     def convert_document(self, fp: Path):
         pass
 
-    def __enter__(self):
-        return self.data_points
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+    @contextmanager
+    def document_set_reader(self):
+        yield self.__iter__()
 
     def __iter__(self):
-        for record in self.data_points:
-            yield record
+        data_files = Path(self.document_location).rglob(f"*.{self.document_extension}")
+        for fpath in data_files:
+            for record in self.convert_document(fpath):
+                yield record
