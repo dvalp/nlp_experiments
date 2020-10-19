404b6b0442c34b1a0ccac7e5c3f426d1c0fd9ff9
David Valpey <david.valpey@viqtordavis.com>
2020-10-20 00:27:36 +0200

    Atticus data reader changes

--- src/dataset_readers/atticus_contract_reader.py
+++ src/dataset_readers/atticus_contract_reader.py
@@ -7,11 +7,11 @@ import requests
 from dataset_readers.dataset_reader import DatasetReader
 
 
-class AtticusContractReader(DatasetReader):
+class AtticusIndividualClauseReader(DatasetReader):
     def __init__(
             self,
-            document_location: str = "../data/atticus-contracts",
-            document_extension: str = "csv",
+            document_location: str = "../data/atticus-contracts/Final Publication/individual_contract_clauses",
+            document_extension: str = ".csv",
             atticus_zip = "aok_beta.zip"
     ):
         self.atticus_zip = atticus_zip
@@ -20,7 +20,7 @@ class AtticusContractReader(DatasetReader):
     def convert_document(self, fp: Path) -> NamedTuple:
         pass
 
-    def download_file(self):
+    def download_datafile(self):
         dl_path = "https://zenodo.org/record/4064880/files/aok_beta.zip?download=1"
         file_path = Path(self.document_location, self.atticus_zip)
         Path(self.document_location).mkdir(exist_ok=True, parents=True)
@@ -28,9 +28,9 @@ class AtticusContractReader(DatasetReader):
         with requests.get(dl_path, stream=True) as r:
             Path(file_path).write_bytes(r.content)
 
-    def extract_file(self):
+    def extract_datafile(self):
         file_path = Path(self.document_location, self.atticus_zip)
         if not file_path.exists():
-            self.download_file()
+            self.download_datafile()
         with ZipFile(file_path, mode='r') as z:
             z.extractall(path=self.document_location)
