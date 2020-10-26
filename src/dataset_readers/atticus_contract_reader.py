305e87d67eb8e266394d022543f1a8ffda33492a
David Valpey <david.valpey@viqtordavis.com>
2020-10-26 01:31:22 +0100

    New changes to Atticus data reader, still incomplete

--- src/dataset_readers/atticus_contract_reader.py
+++ src/dataset_readers/atticus_contract_reader.py
@@ -1,4 +1,5 @@
 import csv
+from itertools import zip_longest
 from pathlib import Path
 from typing import NamedTuple
 from zipfile import ZipFile
@@ -24,22 +25,27 @@ class AtticusIndividualClauseReader(DatasetReader):
             csv_reader = csv.reader(csv_file)
             _ = next(csv_reader)
             for row in csv_reader:
-                fields = list(zip(headers, row))
-                fields.append(("file_path", str(fp)))
+                clause_data = {"file_path": str(fp.resolve()), "clause_text": row[0]}
 
-                record = self.convert_data_types(dict(fields))
+                # Magic that groups list items into pairs
+                annotation_pairs = list(zip_longest(*[iter(row[1:])] * 2))
 
-                yield AtticusIndividualClause(**record)
+                clause_data.update({label_mappings[label_name]: value for label_name, value in annotation_pairs})
+                self.convert_data_types(clause_data)
+                yield AtticusIndividualClause(**clause_data)
 
-    def convert_data_types(self, record: dict):
+    def convert_data_types(self, record: dict) -> dict:
         actions = (
             (int, {}),
-            (bool, {("better_terms non_compete exclusivity customer_solicitation employee_solicitation "
-                     "non_disparagement convenience_termination first_rights change_control anti_assignment "
-                     "profit_sharing price_restriction minimum_commitment volume_restriction ip_assignment joint_ip "
-                     "license_grant non_transferable_license affiliate_licensor affiliate_licensee unlimited_usage "
-                     "perpetual_license code_escrow post_termination audit_rights uncapped_liability capped_liability "
-                     "liquidated_damages warranty_duration insurance outside_claims third_party").split()}),
+            (convert_bool, set("better_terms non_compete exclusivity customer_solicitation "
+                               "employee_solicitation non_disparagement convenience_termination "
+                               "first_rights change_control anti_assignment profit_sharing "
+                               "price_restriction minimum_commitment volume_restriction "
+                               "ip_assignment joint_ip license_grant non_transferable_license "
+                               "affiliate_licensor affiliate_licensee unlimited_usage "
+                               "perpetual_license code_escrow post_termination audit_rights "
+                               "uncapped_liability capped_liability liquidated_damages "
+                               "warranty_duration insurance outside_claims third_party".split())),
             (self.partial_strptime, {})
         )
 
@@ -65,6 +71,10 @@ class AtticusIndividualClauseReader(DatasetReader):
             z.extractall(path=self.document_location)
 
 
+def convert_bool(value):
+    return True if value.lower() == "yes" else False
+
+
 if __name__ == '__main__':
     c = AtticusIndividualClauseReader()
-    print(next(c))
+    print(next(c), '\n')
