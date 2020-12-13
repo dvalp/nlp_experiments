ded2ffdd761b13cefe4aefcc2e36d19833d934e4
David Valpey <david.valpey@viqtordavis.com>
2020-12-13 17:32:55 +0100

    Docstring

--- src/OCR/uitspraak_ocr.py
+++ src/OCR/uitspraak_ocr.py
@@ -4,7 +4,16 @@ import pdf2image
 import pytesseract
 
 
-def ocr_document(pdf_path: Path):
+def ocr_document(pdf_path: Path) -> list[str]:
+    """
+    Use poppler (in pdf2image) to provide images of pdfs stored in memory. No
+    files are stored permanently on the drive. This is probably an improvement
+    over the old version that created and stored images for all PDFs on the
+    drive.
+
+    :param pdf_path: Path to the PDF being converted
+    :return: List of strings, with a string for each page.
+    """
     images = pdf2image.convert_from_path(pdf_path)
     texts = [pytesseract.image_to_string(image, lang="nld") for image in images]
     return texts
