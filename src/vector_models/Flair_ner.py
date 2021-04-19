355464573b6621b75d59b35a4a93aa6992ca6cb4
David Valpey <david.valpey@viqtordavis.com>
2021-04-19 13:57:52 +0200

    Add beginning of result evaluation - visual for now because tag names don't always align well

--- src/vector_models/Flair_ner.py
+++ src/vector_models/Flair_ner.py
@@ -1,14 +1,16 @@
+from collections import Counter
+
 from flair.data import Sentence
 from flair.models import SequenceTagger
 from transformers import pipeline
 
 from conll2002.conll2002 import prep_sentences
 
+hf_tagger = pipeline("ner", model="wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner")
+flair_tagger = SequenceTagger.load("flair/ner-dutch-large")
+
 
 def add_ner_predictions():
-    hf_tagger = pipeline("ner", model="wietsedv/bert-base-dutch-cased-finetuned-conll2002-ner")
-    flair_tagger = SequenceTagger.load("flair/ner-dutch-large")
-
     for sent in prep_sentences():
         sent["hf"] = hf_tagger(sent["sentence"])
 
@@ -18,6 +20,16 @@ def add_ner_predictions():
         sent["flair"] = sentence.get_spans()
 
         yield sent
+
+
+def evaluate_flair():
+    for sent in prep_sentences():
+        sentence = Sentence(sent["sentence"])
+        flair_tagger.predict(sentence)
+        flair_names = Counter(f"{span.text} {span.tag}" for span in sentence.get_spans("ner"))
+        valid_names = Counter(f"{span.entity} {span.etype}" for span in sent["entities"])
+        print("Good names:", valid_names)
+        print("Pred names:", flair_names)
 
 
 if __name__ == '__main__':
