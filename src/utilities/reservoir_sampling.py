5c0b7f3e53b7d815ad830ea59c3858ef89067ccc
David Valpey <david.valpey@viqtordavis.com>
2021-10-29 00:11:48 +0200

    Finish simple reservoir sampling

--- src/utilities/reservoir_sampling.py
+++ src/utilities/reservoir_sampling.py
@@ -1,6 +1,7 @@
 """
 Sample of reservoir sampling methods
 """
+from random import randint
 from typing import Iterator
 
 
@@ -9,3 +10,10 @@ def simple_reservoir(sequence: Iterator, reservoir_size: int):
     sequence = iter(sequence)
     while len(reservoir) < reservoir_size:
         reservoir.append(next(sequence))
+
+    for idx, value in enumerate(sequence, start=reservoir_size):
+        rand_int = randint(0, idx)
+        if rand_int < reservoir_size:
+            reservoir[rand_int] = value
+
+    return reservoir
