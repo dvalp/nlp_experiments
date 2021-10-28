e06595e0bed2df3b6a2c27f42824b430afb5d674
David Valpey <david.valpey@viqtordavis.com>
2021-10-29 00:16:17 +0200

    Docstring

--- src/utilities/reservoir_sampling.py
+++ src/utilities/reservoir_sampling.py
@@ -6,6 +6,17 @@ from typing import Iterator
 
 
 def simple_reservoir(sequence: Iterator, reservoir_size: int):
+    """
+    Naive version simple visits every number and generates a random indeex at
+    each step.
+
+    More efficient methods are possible. Modification may be necessary to read
+    from actual streams.
+
+    :param sequence: The sequence to be sampled
+    :param reservoir_size: Number of items to sample
+    :return: The random sample collected from the stream
+    """
     reservoir = list()
     sequence = iter(sequence)
     while len(reservoir) < reservoir_size:
